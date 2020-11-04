379cdb345babb1162461b0dc3fd75b3f6e402dad
Sachin <sachinpatil@lsssoftware.com>
2020-11-04 12:06:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample_Jenkins_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
